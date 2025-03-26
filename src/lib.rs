6a8aab1fb3a12e6ea29c3fe42d13c8e6c555420f
ZihanType <ZihanType@protonmail.com>
2025-03-26 10:39:38 +0800

    correctly set the `NotFound` page

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ use leptos::prelude::*;
 use leptos_router::{components::*, path};
 
 // Top-Level pages
-use crate::pages::home::Home;
+use crate::pages::{home::Home, not_found::NotFound};
 
 /// An app router which renders the homepage and handles 404's
 #[component]
 pub fn App() -> impl IntoView {
     view! {
         <Router>
-            <Routes fallback=|| view! { NotFound }>
+            <Routes fallback=|| view! { <NotFound /> }>
                 <Route path=path!("/") view=Home />
             </Routes>
         </Router>
